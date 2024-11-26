edf8a2889cfbb530c2e2a07da6c2cf4bb0295f48
tipsydango <tips7511@yahoo.co.uk>
2024-11-26 18:13:19 +0100

    Update App.jsx

--- choco-history/src/App.jsx
+++ choco-history/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Facts from "./Facts";
 import Hero from "./Hero";
 import TestAnimation from "./TestAnimation";
+import './assets/animista.css';
 export default function App() {
   return (
     <div>
